6829550f442c35d4ca69004e5e41c9f66f1fc6fa
Khobizi Ilyes <ilyes@yib.io>
2022-08-23 20:08:14 +0100

    Added axios main client that supports presistent cookies.

---
+++ src/presistentClient.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { CookieJar } from 'tough-cookie';
+import { wrapper } from 'axios-cookiejar-support';
+
+export function presistentClient(
+  extraConfig: axios.AxiosRequestConfig<any> = {},
+): axios.AxiosInstance {
+  const jar = new CookieJar();
+  const client = wrapper(axios.default.create({ jar, ...extraConfig }));
+
+  return client;
+}
